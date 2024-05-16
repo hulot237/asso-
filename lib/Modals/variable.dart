c215981fc24ff3bbd6c9ef92c6077c93b1a7e16a
kengnedjoussehulot <kengnedjoussehulot@gmail.com>
2024-05-16 17:55:23 +0100

    update app version

--- lib/Modals/variable.dart
+++ lib/Modals/variable.dart
@@ -1,11 +1,11 @@
 import 'package:faroty_association_1/localStorage/localCubit.dart';
 
 class Variables {
-  // static String LienAIP = 'https://api.groups.faroty.com';
-  // static String LienInvit = 'https://groups.faroty.com';
+  static String LienAIP = 'https://api.groups.faroty.com';
+  static String LienInvit = 'https://groups.faroty.com';
 
-  static String LienAIP = 'https://api.group.rush.faroty.com';
-  static String LienInvit = 'https://group.rush.faroty.com';
+  // static String LienAIP = 'https://api.group.rush.faroty.com';
+  // static String LienInvit = 'https://group.rush.faroty.com';
 
 
   List urlcodes=[
@@ -14,7 +14,7 @@ class Variables {
 
   String codeMembre = "${AppCubitStorage().state.membreCode}";
 
-  static String version = "1.8.5";
+  static String version = "1.10.9";
 
   late String? tokenNotification; 
 }
